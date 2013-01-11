c33e28d9481cdb65658f8c86495703c547bd225d
Gourav Tiwari <gourav_tiwari@external.mckinsey.com>
2013-01-11 17:12:24 -0500

    Initial version of to-do application

---
+++ app/controllers/application_controller.js
@@ -0,0 +1,3 @@
+before('protect from forgery', function () {
+  protectFromForgery('07c7367ff0a84c76981cb94e96fe6ebcf7f82a57');
+});
---
+++ config/routes.js
@@ -0,0 +1,9 @@
+exports.routes = function (map) {
+    map.get('/', 'posts#index');
+    map.resources('posts');
+
+    // Generic routes. Add all your routes below this line
+    // feel free to remove generic routes
+    map.all(':controller/:action');
+    map.all(':controller/:action/:id');
+};
